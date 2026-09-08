4650a89fdae5ccab2cff2c2e9642a23f249238b6
JuanP <84058843+JuanMontoyaP@users.noreply.github.com>
2026-09-08 16:57:47 -0500

    Include output on step function module

--- step_function/output.tf
+++ step_function/output.tf
@@ -7,3 +7,13 @@ output "state_machine_name" {
   description = "The name of the Step Function state machine"
   value       = aws_sfn_state_machine.this.name
 }
+
+output "cw_lg_arn" {
+  description = "The ARN of the CloudWatch log group for the Step Function state machine"
+  value       = aws_cloudwatch_log_group.this.arn
+}
+
+output "cw_lg_name" {
+  description = "The name of the CloudWatch log group for the Step Function state machine"
+  value       = aws_cloudwatch_log_group.this.name
+}
